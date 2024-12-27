1493aa47cf42de1a1a71ca308d82d7b154045f54
GunYeng <44524521+GunYeng@users.noreply.github.com>
2024-12-27 12:44:54 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.aediu.top"];
-pub const PUBLIC_RS_PUB_KEY: &str = "ATsfCn47hgBQiHVtzkSMBZfjxtNd3TGcfQcyHlGBGNQ=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["al.aediu.top"];
+pub const PUBLIC_RS_PUB_KEY: &str = "+tOS7k3TO+pW3rpZCOQ06d9KLV1A56ZqdIYMmpZQL2Y=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
